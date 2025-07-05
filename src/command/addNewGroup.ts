a3e59cfa10c3c5d0d8d655b1b6fa7353389597da
huyunan <18842920976@163.com>
2025-07-05 18:47:17 +0800

    删除分组功能完善

--- src/command/addNewGroup.ts
+++ src/command/addNewGroup.ts
@@ -5,6 +5,7 @@ import configMgr from '../helper/configMgr'
 import { DEFAULT_GROUP } from '../enum'
 import localize from '../helper/localize'
 import { getFirstGitRepository, getGitBranchName } from '../helper/util'
+import { ItemInSettingsJson } from '../model'
 
 export function addNewGroup(favouriteProvider: FavouriteProvider) {
   return vscode.commands.registerCommand('favourite.group.newGroup', async function (value: Resource) {
@@ -41,12 +42,23 @@ export function addNewGroup(favouriteProvider: FavouriteProvider) {
 
 function deleteCurrentGroup(previousGroups) {
   const currentGroup = configMgr.get('currentGroup')
+  const resources = (configMgr.get('resources') as Array<ItemInSettingsJson>) || []
   const index = previousGroups.indexOf(currentGroup)
   if (currentGroup === localize('ext.default')) {
     vscode.window.showErrorMessage(localize('msg.delete.default.group'));
   } else if (index !== -1) {
     previousGroups.splice(index, 1)
-    configMgr.save([{key: 'groups', value: previousGroups}, {key: 'currentGroup', value: localize('ext.default')}]);
+    const newResources: Array<ItemInSettingsJson> = resources.filter(item => item.group != currentGroup)
+    let newCurrentGroup = previousGroups[index - 1]
+    if (previousGroups.length > index) {
+      newCurrentGroup = previousGroups[index]
+    }
+    const saveItems = [
+      {key: 'groups', value: previousGroups},
+      {key: 'currentGroup', value: newCurrentGroup},
+      {key: 'resources', value: newResources}
+    ]
+    configMgr.save(saveItems);
   } else {
     vscode.window.showErrorMessage(`${localize('msg.group.not.exist.left')} "${currentGroup}" ${localize('msg.group.not.exist.right')}`);
   }
